744d2e7f5e76c683a620f78f5a274dd1b7a8be32
Kosimjon.Kosimov <kosimjon.kosimov@megafon.tj>
2023-06-18 00:40:53 +0500

    Remove dist and output from gitignore

---
+++ .output/server/chunks/app/client.manifest.mjs
@@ -0,0 +1,119 @@
+const client_manifest = {
+  "assets/fonts/DancingScript-Bold.ttf": {
+    "resourceType": "font",
+    "mimeType": "font/ttf",
+    "file": "DancingScript-Bold.5a573995.ttf",
+    "src": "assets/fonts/DancingScript-Bold.ttf"
+  },
+  "assets/fonts/DancingScript-Medium.ttf": {
+    "resourceType": "font",
+    "mimeType": "font/ttf",
+    "file": "DancingScript-Medium.8cc56496.ttf",
+    "src": "assets/fonts/DancingScript-Medium.ttf"
+  },
+  "assets/fonts/DancingScript-Regular.ttf": {
+    "resourceType": "font",
+    "mimeType": "font/ttf",
+    "file": "DancingScript-Regular.49abeae0.ttf",
+    "src": "assets/fonts/DancingScript-Regular.ttf"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.css": {
+    "resourceType": "style",
+    "file": "error-404.23f2309d.css",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.css"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "error-404.5855429c.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue"
+  },
+  "error-404.23f2309d.css": {
+    "file": "error-404.23f2309d.css",
+    "resourceType": "style"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.css": {
+    "resourceType": "style",
+    "file": "error-500.aa16ed4d.css",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.css"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "error-500.45999427.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+  },
+  "error-500.aa16ed4d.css": {
+    "file": "error-500.aa16ed4d.css",
+    "resourceType": "style"
+  },
+  "node_modules/nuxt/dist/app/entry.css": {
+    "resourceType": "style",
+    "file": "entry.cc36bdd8.css",
+    "src": "node_modules/nuxt/dist/app/entry.css"
+  },
+  "node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "assets": [
+      "DancingScript-Regular.49abeae0.ttf",
+      "DancingScript-Medium.8cc56496.ttf",
+      "DancingScript-Bold.5a573995.ttf"
+    ],
+    "css": [
+      "entry.cc36bdd8.css"
+    ],
+    "dynamicImports": [
+      "virtual:nuxt:C:/Users/kosimjon.kosimov/Desktop/note-pad/.nuxt/error-component.mjs"
+    ],
+    "file": "entry.bf2005f6.js",
+    "isEntry": true,
+    "src": "node_modules/nuxt/dist/app/entry.js"
+  },
+  "entry.cc36bdd8.css": {
+    "file": "entry.cc36bdd8.css",
+    "resourceType": "style"
+  },
+  "DancingScript-Regular.49abeae0.ttf": {
+    "file": "DancingScript-Regular.49abeae0.ttf",
+    "resourceType": "font",
+    "mimeType": "font/ttf"
+  },
+  "DancingScript-Medium.8cc56496.ttf": {
+    "file": "DancingScript-Medium.8cc56496.ttf",
+    "resourceType": "font",
+    "mimeType": "font/ttf"
+  },
+  "DancingScript-Bold.5a573995.ttf": {
+    "file": "DancingScript-Bold.5a573995.ttf",
+    "resourceType": "font",
+    "mimeType": "font/ttf"
+  },
+  "virtual:nuxt:C:/Users/kosimjon.kosimov/Desktop/note-pad/.nuxt/error-component.mjs": {
+    "resourceType": "script",
+    "module": true,
+    "dynamicImports": [
+      "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ],
+    "file": "error-component.eb08874d.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "virtual:nuxt:C:/Users/kosimjon.kosimov/Desktop/note-pad/.nuxt/error-component.mjs"
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
